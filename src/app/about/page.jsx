44f0e53835dc72eafa17d7e1dec7a9f6f7cf19c5
ej84 <ej84@njit.edu>
2025-01-24 16:44:07 -0500

    .

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -19,9 +19,9 @@ const page = () => {
         <div className="grid grid-rows-1 p-5 gap-4 mt-10 justify-center items-center text-center text-sm lg:text-base shadow-xl shadow-purple-700">
           <h1>이름: 요미냥 / 본명: 이시은</h1>
           <h2>직업: 코스어, 숲tv 스트리머, 인플루언서</h2>
-          <p>방송 경력: 2018년 9월 10일 ~ 현재</p>
+          <p>방송 경력: 트위치 - 2022년 2월 7일부터 2023년 12월 6일까지<br />SOOP - 2023년 12월 7일 + 403일 </p>
           <p>대표 코스프레 캐릭터: 아리(리그 오브 레전드), 원신 - 감우/요이미야/닐루, D.Va(오버워치), 샤이릴리(해외 버튜버) 등등등</p>
-          <p>특기사항:</p>
+          <p>특기사항:뛰어난 코스프레 능력자이며 스트리머 동료들, 그리고 시청자들과 함께 재밌는 방송을 만들어가기 위해 노력을 아끼지 않는 훌륭한 인플루언서입니다.</p>
         </div>
       </main>
       <Footer />
